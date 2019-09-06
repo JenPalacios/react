6cf42063fe846512b1d9b430d5f1fc531913b846
Jennifer <palacios.jennifer@gmail.com>
2019-09-06 16:15:50 +0200

    add first 13 lessons

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+
+    //2. Add our new fish to that fishes variable inside the copy
+    fishes[`fish${Date.now()}`] = fish;
+
+    //3. Set the new fishes object to state
+    this.setState({
+      // No need to use both words like fishes: fishes
+      fishes
+    });
+  };
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
+}
+
+export default App;
