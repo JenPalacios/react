78a433ba766aa0eb65dbc598c650ef3b67fbb715
Jennifer <palacios.jennifer@gmail.com>
2019-09-08 22:20:37 +0200

    move variables to env

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWMZCa0OoLjytJxo2mP0BMZ7XvoKb9aD0",
-  authDomain: "catch-of-the-day-jencios.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-jencios.firebaseio.com"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
